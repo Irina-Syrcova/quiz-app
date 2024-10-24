363ba116c33cad8ff64c4de04c9052a06fb25747
Irina Syrcova <irinasyrcova223@gmail.com>
2024-10-24 16:55:39 +0300

    deploy

--- src/components/ui/card/card.jsx
+++ src/components/ui/card/card.jsx
@@ -15,7 +15,7 @@ export const Card = () => {
   if (isWelcome) {
     return (
       <>
-        <img className={styles.welcome} src="/images/sali.png" alt="Welcome" />
+        <img className={styles.welcome} src="./images/sali.png" alt="Welcome" />
         <div className={styles.container}>
           <h1 className={styles.title}>Добро пожаловать </h1>
           <p className={styles.subtitle}>на викторину по странам и столицам!</p>
@@ -36,7 +36,7 @@ export const Card = () => {
         <path d="M24.7763 16.0624C25.2018 15.6368 25.2018 14.9458 24.7763 14.5203C24.3508 14.0948 23.6598 14.0948 23.2343 14.5203L19.6498 18.1082L16.0619 14.5237C15.6364 14.0982 14.9453 14.0982 14.5198 14.5237C14.0943 14.9492 14.0943 15.6402 14.5198 16.0658L18.1077 19.6503L14.5232 23.2382C14.0977 23.6637 14.0977 24.3547 14.5232 24.7802C14.9487 25.2057 15.6398 25.2057 16.0653 24.7802L19.6498 21.1923L23.2377 24.7768C23.6632 25.2023 24.3542 25.2023 24.7797 24.7768C25.2052 24.3513 25.2052 23.6603 24.7797 23.2348L21.1918 19.6503L24.7763 16.0624Z"/>
         </svg>
         <div className={styles.question}>
-          <img className={styles.flag} src="/images/flag.jpg" alt="img" />
+          <img className={styles.flag} src="./images/flag.jpg" alt="img" />
           <p className={styles.subtitle}>Флаг какой страны изображен?</p>
         </div>
         <ul className={styles.list}>
@@ -66,7 +66,7 @@ export const Card = () => {
   if (isResult) {
     return (
       <>
-        <img className={styles.result} src="/images/result.png" alt="img" />
+        <img className={styles.result} src="./images/result.png" alt="img" />
         <div className={styles.results}>
           <h2 className={styles.title}>Результат</h2>
           {isTrue && isFalse ? (
--- src/components/ui/counter/counter.jsx
+++ src/components/ui/counter/counter.jsx
@@ -4,11 +4,11 @@ export const Counter = () => {
   return (
     <div className={styles.counter}>
       <button className={styles.button}>
-        <img src="/images/minus.svg" alt="минус" />
+        <img src="./images/minus.svg" alt="минус" />
       </button>
       <input className={styles.input} type="number" placeholder='18' />
       <button className={styles.button}>
-      <img src="/images/plus.svg" alt="плюс" />
+      <img src="./images/plus.svg" alt="плюс" />
       </button>
     </div>
   )
